f525f4fbae06f0dc3943d4b32f3ebaef2c1b66da
keyboardcat <azhar.gana@yahoo.com>
2024-09-20 09:40:02 +0200

    fix(static): introduced Content-Type

--- src/main/java/io/github/keyboardcat1/minertc/web/StaticServlet.java
+++ src/main/java/io/github/keyboardcat1/minertc/web/StaticServlet.java
@@ -15,12 +15,16 @@ class StaticServlet extends HttpServlet {
         //serve resources/static/*
         String fileName = req.getPathInfo();
         InputStream is = this.getClass().getClassLoader().getResourceAsStream("web/static" + fileName);
-
         if (is == null) {
             res.sendError(404);
             return;
         }
-
+        String ext = fileName.substring(fileName.lastIndexOf('.'));
+        if (ext.equals(".js")) {
+            res.setContentType("text/javascript");
+        } else if (ext.equals(".css")) {
+            res.setContentType("text/css");
+        }
         is.transferTo(res.getOutputStream());
     }
 }
